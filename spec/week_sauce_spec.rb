3515d3433818b942f803c278bf3e2488241101bf
Dante Piombino <dan-ding@users.noreply.github.com>
2017-08-04 16:27:49 -0400

    update rspec syntax

--- spec/week_sauce_spec.rb
+++ spec/week_sauce_spec.rb
@@ -13,19 +13,19 @@ describe WeekSauce do
   
   describe "initializer" do
     it "defaults to zero/blank" do
-      WeekSauce.new.to_i.should == 0
-      WeekSauce.new(nil).to_i.should == 0
+      expect(WeekSauce.new.to_i).to eq 0
+      expect(WeekSauce.new(nil).to_i).to eq 0
     end
     
     it "accepts valid numbers" do
-      WeekSauce.new(0).to_i.should   == 0
-      WeekSauce.new(64).to_i.should  == 64
-      WeekSauce.new(max).to_i.should == max
+      expect(WeekSauce.new(0).to_i).to   eq 0
+      expect(WeekSauce.new(64).to_i).to  eq 64
+      expect(WeekSauce.new(max).to_i).to eq max
     end
     
     it "clamps input" do
-      WeekSauce.new(-100).to_i.should == 0
-      WeekSauce.new(2**9).to_i.should == max
+      expect(WeekSauce.new(-100).to_i).to eq 0
+      expect(WeekSauce.new(2**9).to_i).to eq max
     end
   end
   
@@ -33,13 +33,13 @@ describe WeekSauce do
     context "with no days set" do
       let(:week) { WeekSauce.new }
       it "returns correct values" do
-        week.blank?.should  be_true
-        week.any?.should    be_false
-        week.many?.should   be_false
-        week.one?.should    be_false
-        week.all?.should    be_false
+        expect(week.blank?).to  be_truthy
+        expect(week.any?).to    be_falsey
+        expect(week.many?).to   be_falsey
+        expect(week.one?).to    be_falsey
+        expect(week.all?).to    be_falsey
         days.each do |day|
-          week.send("#{day.to_s}?").should be_false
+          expect(week.send("#{day.to_s}?")).to be_falsey
         end
       end
     end
@@ -47,13 +47,13 @@ describe WeekSauce do
     context "with only one day set" do
       let(:week) { WeekSauce.new(16) } # Thursday
       it "returns correct values" do
-        week.blank?.should  be_false
-        week.any?.should    be_true
-        week.many?.should   be_false
-        week.one?.should    be_true
-        week.all?.should    be_false
+        expect(week.blank?).to  be_falsey
+        expect(week.any?).to    be_truthy
+        expect(week.many?).to   be_falsey
+        expect(week.one?).to    be_truthy
+        expect(week.all?).to    be_falsey
         days.each do |day|
-          week.send("#{day.to_s}?").should == (day == :thursday)
+          expect(week.send("#{day.to_s}?")).to eq (day == :thursday)
         end
       end
     end
@@ -61,16 +61,16 @@ describe WeekSauce do
     context "with more than one day set" do
       let(:week) { WeekSauce.new(31) } # Sunday-Thursday
       it "returns correct values" do
-        week.blank?.should  be_false
-        week.any?.should    be_true
-        week.many?.should   be_true
-        week.one?.should    be_false
-        week.all?.should    be_false
+        expect(week.blank?).to  be_falsey
+        expect(week.any?).to    be_truthy
+        expect(week.many?).to   be_truthy
+        expect(week.one?).to    be_falsey
+        expect(week.all?).to    be_falsey
         days[0..4].each do |day|
-          week.send("#{day.to_s}?").should be_true
+          expect(week.send("#{day.to_s}?")).to be_truthy
         end
         days[5..6].each do |day|
-          week.send("#{day.to_s}?").should be_false
+          expect(week.send("#{day.to_s}?")).to be_falsey
         end
       end
     end
@@ -78,13 +78,13 @@ describe WeekSauce do
     context "with all days set" do
       let(:week) { WeekSauce.new(max) }
       it "returns correct values" do
-        week.blank?.should  be_false
-        week.any?.should    be_true
-        week.many?.should   be_true
-        week.one?.should    be_false
-        week.all?.should    be_true
+        expect(week.blank?).to  be_falsey
+        expect(week.any?).to    be_truthy
+        expect(week.many?).to   be_truthy
+        expect(week.one?).to    be_falsey
+        expect(week.all?).to    be_truthy
         days.each do |day|
-          week.send("#{day.to_s}?").should be_true
+          expect(week.send("#{day.to_s}?")).to be_truthy
         end
       end
     end
@@ -96,85 +96,85 @@ describe WeekSauce do
       it "works with integers" do
         week[0] = true
         week[0] = true # repeated on purpose to expose bitwise XOR errors
-        week.to_i.should == 1
+        expect(week.to_i).to eq 1
         week[6] = true
-        week.to_i.should == 65
+        expect(week.to_i).to eq 65
       end
       
       it "works with integer strings" do
         week["0"] = true
-        week.to_i.should == 1
+        expect(week.to_i).to eq 1
         week["5"] = true
-        week.to_i.should == 33
+        expect(week.to_i).to eq 33
       end
       
       it "works with day-name strings (upper- and lowercase)" do
         week["sunday"] = true
-        week.to_i.should == 1
+        expect(week.to_i).to eq 1
         week["THURSDAY"] = true
-        week.to_i.should == 17
+        expect(week.to_i).to eq 17
         week["MoNdAy"] = true
-        week.to_i.should == 19
+        expect(week.to_i).to eq 19
       end
       
       it "ignores out-of-bounds integers" do
         week[-1] = true
-        week.to_i.should == 0
+        expect(week.to_i).to eq 0
         week[7] = true
-        week.to_i.should == 0
+        expect(week.to_i).to eq 0
       end
       
       it "works with day-name symbols" do
         week[:sunday] = true
-        week.to_i.should == 1
+        expect(week.to_i).to eq 1
         week[:wednesday] = true
-        week.to_i.should == 9
+        expect(week.to_i).to eq 9
       end
       
       it "ignores non-sensical symbols" do
         week[:foo] = true
-        week.to_i.should == 0
+        expect(week.to_i).to eq 0
       end
       
       it "works with Time and TimeWithZone objects" do
         week[Time.now] = true
-        week.to_i.should == 2**Time.now.wday
+        expect(week.to_i).to eq 2**Time.now.wday
         
         Time.zone = ActiveSupport::TimeZone["Copenhagen"]
         time = Time.zone.now
         week[time] = true
-        week.to_i.should == 2**time.wday
+        expect(week.to_i).to eq 2**time.wday
       end
       
       it "works with Date and DateTime objects" do
         date = Date.today
         week[date] = true
-        week.to_i.should == 2**date.wday
+        expect(week.to_i).to eq 2**date.wday
         
         date = DateTime.now
         week[date] = true
-        week.to_i.should == 2**date.wday
+        expect(week.to_i).to eq 2**date.wday
       end
       
       it "ignores unhandled argument types" do
         week["string"] = true
-        week.to_i.should == 0
+        expect(week.to_i).to eq 0
       end
     end
     
     it "works with day-name setters" do
       week = WeekSauce.new
       week.sunday = true
-      week.to_i.should == 1
+      expect(week.to_i).to eq 1
       week.saturday = true
-      week.to_i.should == 65
+      expect(week.to_i).to eq 65
     end
     
     describe "using the set method" do
       it "sets multiple days" do
         week = WeekSauce.new(42)
         week.set 0, :tuesday, :thursday, 6
-        week.all?.should be_true
+        expect(week.all?).to be_truthy
       end
     end
     
@@ -182,12 +182,12 @@ describe WeekSauce do
       it "sets multiple days, and only those days" do
         week = WeekSauce.new(42)
         week.set! 0, :tuesday
-        week.to_i.should == 5
+        expect(week.to_i).to eq 5
       end
       
       it "returns self" do
         week = WeekSauce.new(42)
-        week.set!(:tuesday).should be(week)
+        expect(week.set!(:tuesday)).to be(week)
       end
     end
     
@@ -195,7 +195,7 @@ describe WeekSauce do
       it "unsets multiple days" do
         week = WeekSauce.new(42)
         week.unset 1, :wednesday, :friday
-        week.blank?.should be_true
+        expect(week.blank?).to be_truthy
       end
     end
     
@@ -203,12 +203,12 @@ describe WeekSauce do
       it "unsets multiple days, and only those days" do
         week = WeekSauce.new(42)
         week.unset! :monday, :wednesday, 4, :friday, 6
-        week.to_i.should == 5
+        expect(week.to_i).to eq 5
       end
       
       it "returns self" do
         week = WeekSauce.new(42)
-        week.unset!(:monday).should be(week)
+        expect(week.unset!(:monday)).to be(week)
       end
     end
   end
@@ -218,102 +218,102 @@ describe WeekSauce do
       let(:week) { WeekSauce.new(42) } # Monday, Wednesday, Friday
       
       it "works with integers" do
-        week[0].should be_false
-        week[1].should be_true
-        week[2].should be_false
-        week[3].should be_true
-        week[4].should be_false
-        week[5].should be_true
-        week[6].should be_false
+        expect(week[0]).to be_falsey
+        expect(week[1]).to be_truthy
+        expect(week[2]).to be_falsey
+        expect(week[3]).to be_truthy
+        expect(week[4]).to be_falsey
+        expect(week[5]).to be_truthy
+        expect(week[6]).to be_falsey
       end
       
       it "returns nil for out-of-bounds integers" do
-        week[-1].should be_nil
-        week[7].should be_nil
+        expect(week[-1]).to be_nil
+        expect(week[7]).to be_nil
       end
       
       it "works with integer strings" do
-        week["0"].should be_false
-        week["1"].should be_true
-        week["2"].should be_false
-        week["3"].should be_true
-        week["4"].should be_false
-        week["5"].should be_true
-        week["6"].should be_false
+        expect(week["0"]).to be_falsey
+        expect(week["1"]).to be_truthy
+        expect(week["2"]).to be_falsey
+        expect(week["3"]).to be_truthy
+        expect(week["4"]).to be_falsey
+        expect(week["5"]).to be_truthy
+        expect(week["6"]).to be_falsey
       end
       
       it "works with day-name strings (upper- and lowercase)" do
-        week["sunday"].should be_false
-        week["MONDAY"].should be_true
-        week["tuesday"].should be_false
-        week["WeDnEsDaY"].should be_true
-        week["thursday"].should be_false
-        week["friDAY"].should be_true
-        week["SATURday"].should be_false
+        expect(week["sunday"]).to be_falsey
+        expect(week["MONDAY"]).to be_truthy
+        expect(week["tuesday"]).to be_falsey
+        expect(week["WeDnEsDaY"]).to be_truthy
+        expect(week["thursday"]).to be_falsey
+        expect(week["friDAY"]).to be_truthy
+        expect(week["SATURday"]).to be_falsey
       end
       
       it "returns nil for non-sensical strings" do
-        week["bacon"].should be_nil
-        week["monkey"].should be_nil
+        expect(week["bacon"]).to be_nil
+        expect(week["monkey"]).to be_nil
       end
       
       it "works with day-name symbols" do
-        week[:sunday].should be_false
-        week[:monday].should be_true
-        week[:tuesday].should be_false
-        week[:wednesday].should be_true
-        week[:thursday].should be_false
-        week[:friday].should be_true
-        week[:saturday].should be_false
+        expect(week[:sunday]).to be_falsey
+        expect(week[:monday]).to be_truthy
+        expect(week[:tuesday]).to be_falsey
+        expect(week[:wednesday]).to be_truthy
+        expect(week[:thursday]).to be_falsey
+        expect(week[:friday]).to be_truthy
+        expect(week[:saturday]).to be_falsey
       end
       
       it "returns nil for non-sensical symbols" do
-        week[:foo].should be_nil
+        expect(week[:foo]).to be_nil
       end
       
       it "works with Time and TimeWithZone objects" do
         time = Time.now
         time = time + (3 - time.wday).days # set to Wednesday
-        week[time].should be_true
+        expect(week[time]).to be_truthy
         time = time + 1.day # set to Thursday
-        week[time].should be_false
+        expect(week[time]).to be_falsey
         
         Time.zone = ActiveSupport::TimeZone["Copenhagen"]
         time = Time.zone.now
         time = time + (3 - time.wday).days
-        week[time].should be_true
+        expect(week[time]).to be_truthy
         time = time + 1.day # set to Thursday
-        week[time].should be_false
+        expect(week[time]).to be_falsey
       end
       
       it "works with Date and DateTime objects" do
         date = Date.today
         date = date + (3 - date.wday).days # set to Wednesday
-        week[date].should be_true
+        expect(week[date]).to be_truthy
         date = date + 1.day # set to Thursday
-        week[date].should be_false
+        expect(week[date]).to be_falsey
         
         date = DateTime.now
         date = date + (3 - date.wday).days # set to Wednesday
-        week[date].should be_true
+        expect(week[date]).to be_truthy
         date = date + 1.day # set to Thursday
-        week[date].should be_false
+        expect(week[date]).to be_falsey
       end
       
       it "returns nil for unhandled argument types" do
-        week["string"].should be_nil
+        expect(week["string"]).to be_nil
       end
     end
     
     it "works with day-name getters" do
       week = WeekSauce.new(42)
-      week.sunday.should    be_false
-      week.monday.should    be_true
-      week.tuesday.should   be_false
-      week.wednesday.should be_true
-      week.thursday.should  be_false
-      week.friday.should    be_true
-      week.saturday.should  be_false
+      expect(week.sunday).to    be_falsey
+      expect(week.monday).to    be_truthy
+      expect(week.tuesday).to   be_falsey
+      expect(week.wednesday).to be_truthy
+      expect(week.thursday).to  be_falsey
+      expect(week.friday).to    be_truthy
+      expect(week.saturday).to  be_falsey
     end
   end
   
@@ -321,12 +321,12 @@ describe WeekSauce do
     it "clears the instance" do
       week = WeekSauce.new(max)
       week.blank!
-      week.to_i.should == 0
+      expect(week.to_i).to eq 0
     end
     
     it "returns self" do
       week = WeekSauce.new
-      week.blank!.should be(week)
+      expect(week.blank!).to be(week)
     end
   end
   
@@ -334,26 +334,26 @@ describe WeekSauce do
     it "sets all bits" do
       week = WeekSauce.new
       week.all!
-      week.to_i.should == max
+      expect(week.to_i).to eq max
     end
     
     it "returns self" do
       week = WeekSauce.new
-      week.all!.should be(week)
+      expect(week.all!).to be(week)
     end
   end
   
   describe "utilities" do
     describe "to_a" do
       it "returns an array" do
-        WeekSauce.new(42).to_a.should include(:monday, :wednesday, :friday)
-        WeekSauce.new.to_a.should == []
+        expect(WeekSauce.new(42).to_a).to include(:monday, :wednesday, :friday)
+        expect(WeekSauce.new.to_a).to eq([])
       end
     end
     
     describe "to_hash" do
       it "to_hash returns a hash" do
-        WeekSauce.new(42).to_hash.should == {
+        expect(WeekSauce.new(42).to_hash).to  eq({
           sunday:    false,
           monday:    true,
           tuesday:   false,
@@ -361,7 +361,7 @@ describe WeekSauce do
           thursday:  false,
           friday:    true,
           saturday:  false
-        }
+        })
       end
     end
     
@@ -369,34 +369,34 @@ describe WeekSauce do
       let(:week) { WeekSauce.new(rand(max+1)) }
       
       it "dups to a new instance" do
-        week.dup.to_i.should == week.to_i
-        week.dup.should_not be(week)
+        expect(week.dup.to_i).to eq week.to_i
+        expect(week.dup).to_not be(week)
       end
     end
     
     describe "inspect" do
       it "lists bitmask & days" do
         week = WeekSauce.new(42)
-        week.inspect.should == "42: Monday, Wednesday, Friday"
+        expect(week.inspect).to eq "42: Monday, Wednesday, Friday"
       end
       
       it "returns a simple message if no days have been set" do
         week = WeekSauce.new
-        week.inspect.should == "0: No days set"
+        expect(week.inspect).to eq "0: No days set"
       end
       
       it "returns a simple message if all days have been set" do
         week = WeekSauce.new.all!
-        week.inspect.should == "127: All days set"
+        expect(week.inspect).to eq "127: All days set"
       end
     end
     
     describe "count" do
       it "returns the number of days set" do
         week = WeekSauce.new
-        week.count.should == 0
-        week.all!.count.should == 7
-        week.set!(0, 2, 5).count.should == 3
+        expect(week.count).to eq 0
+        expect(week.all!.count).to eq 7
+        expect(week.set!(0, 2, 5).count).to eq 3
       end
     end
   end
@@ -405,13 +405,13 @@ describe WeekSauce do
     let(:week) { WeekSauce.new(42) }
     
     it "works with fixnums" do
-      week.should     == 42
-      week.should_not == 43
+      expect(week).to     eq 42
+      expect(week).to_not eq 43
     end
     
     it "works with other instances" do
-      week.should     == WeekSauce.new(42)
-      week.should_not == WeekSauce.new
+      expect(week).to     eq WeekSauce.new(42)
+      expect(week).to_not eq WeekSauce.new
     end
   end
   
@@ -423,24 +423,24 @@ describe WeekSauce do
         date = Date.today
         offset = 3 - date.wday
         offset += 7 if offset < 0
-        week.next_date.should == date + offset.days
+        expect(week.next_date).to eq date + offset.days
       end
       
       it "finds next date from a given day" do
         date = Time.parse "2013-04-01" # April fool's (also a happens to be a Monday)
-        week.next_date(date).should == date.to_date + 2.days
+        expect(week.next_date(date)).to eq date.to_date + 2.days
       end
       
       it "returns a duplicate of the from argument if it matches" do
         week = WeekSauce.new(3) # Monday
         date = Date.parse "2013-04-01"
         result = week.next_date(date)
-        result.should == date
-        result.should_not be(date)
+        expect(result).to eq date
+        expect(result).to_not be(date)
       end
       
       it "returns nil if the week's blank" do
-        WeekSauce.new.next_date.should be_nil
+        expect(WeekSauce.new.next_date).to be_nil
       end
     end
     
@@ -451,16 +451,16 @@ describe WeekSauce do
         ends   = starts + 3.weeks
         week[starts] = true
         dates = week.dates_in(starts..ends)
-        dates.length.should == 4
-        dates.first.should == starts
-        dates.last.should == ends
+        expect(dates.length).to eq 4
+        expect(dates.first).to eq starts
+        expect(dates.last).to eq ends
       end
       
       it "returns an empty array if the week's blank" do
         week = WeekSauce.new
         starts = Date.today
         ends   = starts + 3.weeks
-        week.dates_in(starts...ends).empty?.should be_true
+        expect(week.dates_in(starts...ends).empty?).to be_truthy
       end
     end
   end
@@ -469,27 +469,27 @@ describe WeekSauce do
     describe "load" do
       it "loads from a string" do
         week = WeekSauce.load("127")
-        week.to_i.should == 127
+        expect(week.to_i).to eq 127
       end
       
       it "clamps value" do
-        WeekSauce.load("1027").to_i.should == 127
-        WeekSauce.load("-127").to_i.should == 0
+        expect(WeekSauce.load("1027").to_i).to eq 127
+        expect(WeekSauce.load("-127").to_i).to eq 0
       end
       
       it "absorbs conversion errors" do
-        WeekSauce.load([]).to_i.should == 0
+        expect(WeekSauce.load([]).to_i).to eq 0
       end
     end
     
     describe "dump" do
       it "dumps to a string" do
         week = WeekSauce.new(42)
-        WeekSauce.dump(week).should == "42"
+        expect(WeekSauce.dump(week)).to eq "42"
       end
       
       it "defaults to outputting zero" do
-        WeekSauce.dump(123).should == "0"
+        expect(WeekSauce.dump(123)).to eq "0"
       end
     end
   end
